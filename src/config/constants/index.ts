fd81251609b9b806be447496b7ec385092185323
halink0803 <halink0803@gmail.com>
2022-09-30 00:14:37 +0700

    change mapping file name

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -4,30 +4,30 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const ADDRESS_LOCK = '0xffffffffffffffffffffffffffffffffffffffff'
 
-export const DMM_DYNAMIC_FEE_FACTORY_ADDRESS = '0x833e4083b7ae46cea85695c4f7ed25cdad8886de'
+export const DMM_DYNAMIC_FEE_FACTORY_ADDRESS = '0x878dfe971d44e9122048308301f540910bbd934c'
 export const DMM_STATIC_FEE_FACTORY_ADDRESS = '0x1c758af0688502e49140230f6b0ebd376d429be5'
 export const DMM_STATIC_FEE_FACTORY_LEGACY_ADDRESS = ''
 
-export const ETH_PRICING_POOLS = '0xd478953d5572f829f457a5052580cbeaee36c1aa|0x20d6b227f4a5a2a13d520329f01bb1f8f9d2d628|0xf8467ef9de03e83b5a778ac858ea5c2d1fc47188|0xce9874c42dce7fffbe5e48b026ff1182733266cb'
+export const ETH_PRICING_POOLS = '0xec303ce1edbebf7e71fc7b350341bb6a6a7a6381'
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
 export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('4000')
 
 // minimum liquidity for price to get tracked
-export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('2') // default is 2
+export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('13') // default is 2
 
-export const WRAPPED_NATIVE_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+export const WRAPPED_NATIVE_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
 
-export const KNC_ADDRESS = '0xdefa4e8a7bcba345f687a2f1456f5edd9ce97202'
+export const KNC_ADDRESS = '0xfe56d5892bdffc7bf58f2e84be1b2c32d21c308b'
 export const KNC_NAME = 'Kyber Network'
 export const KNC_SYMBOL = 'KNC'
 
-export const KNCL_ADDRESS = '0xdd974d5c2e2928dea5f71b9825b8b646686bd200'
+export const KNCL_ADDRESS = '0x7b2810576aa1cce68f2b118cef1f36467c648f92'
 export const KNCL_NAME = 'Kyber Network Legacy'
 export const KNCL_SYMBOL = 'KNCL'
 
 export let FACTORY_BPS = BigDecimal.fromString('10000')
 
-export let NETWORK = 'mainnet'
+export let NETWORK = 'bsc'
 
-export let WHITELISTED_TOKENS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2,0x6b175474e89094c44da98b954eedeac495271d0f,0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48,0xdac17f958d2ee523a2206206994597c13d831ec7,0xdefa4e8a7bcba345f687a2f1456f5edd9ce97202'
+export let WHITELISTED_TOKENS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c,0xe9e7cea3dedca5984780bafc599bd69add087d56,0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d,0x55d398326f99059ff775485246999027b3197955,0xfe56d5892bdffc7bf58f2e84be1b2c32d21c308b,0x2170ed0880ac9a755fd29b2688956bd959f933f8'
